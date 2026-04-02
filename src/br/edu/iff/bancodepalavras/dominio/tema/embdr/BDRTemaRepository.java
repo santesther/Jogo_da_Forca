53aa1ecfcfc0dd9a2b0b51be3774d07ede3b74c3
esther <esthersant.edu@gmail.com>
2026-04-02 08:35:17 -0300

    feat: add BDRTemaRepository

---
+++ src/br/edu/iff/bancodepalavras/dominio/tema/embdr/BDRTemaRepository.java
@@ -0,0 +1,40 @@
+package br.edu.iff.bancodepalavras.dominio.tema.embdr;
+
+import br.edu.iff.bancodepalavras.dominio.tema.Tema;
+import br.edu.iff.bancodepalavras.dominio.tema.TemaRepository;
+import br.edu.iff.repository.RepositoryException;
+
+public class BDRTemaRepository implements TemaRepository {
+
+    private static BDRTemaRepository soleInstance;
+
+    private BDRTemaRepository() {}
+
+    public static BDRTemaRepository getSoleInstance() {
+        if (soleInstance == null) {
+            soleInstance = new BDRTemaRepository();
+        }
+        return soleInstance;
+    }
+
+    @Override
+    public long getProximoId() { return 0; }
+
+    @Override
+    public void inserir(Tema tema) throws RepositoryException {}
+
+    @Override
+    public void atualizar(Tema tema) throws RepositoryException {}
+
+    @Override
+    public void remover(Tema tema) throws RepositoryException {}
+
+    @Override
+    public Tema getPorId(long id) { return null; }
+
+    @Override
+    public Tema[] getPorNome(String nome) { return new Tema[0]; }
+
+    @Override
+    public Tema[] getTodos() { return new Tema[0]; }
+}
